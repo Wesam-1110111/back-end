a8ff7e6c6f211c07012cbaad1247c116b229b0ad
Wesam-1110111 <mrw202065@gmail.com>
2025-01-28 16:00:18 +0200

    Flask

---
+++ Flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+# print(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', title='Home')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/singin')
+def singin():
+    return render_template('singin.html', title='Sing in')
+
+@app.route('/singup')
+def singup():
+    return render_template('singup.html', title='Sing up')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
